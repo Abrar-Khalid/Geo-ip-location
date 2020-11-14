b38773f4c2d0c4e091438a5c126fb19eb8cd6145
SystemCTL <70237548+System-CTL@users.noreply.github.com>
2020-11-15 04:48:38 +0500

    Update ip-location.py

--- ip-location.py
+++ ip-location.py
@@ -1,12 +1,28 @@
 import requests
 import argparse
 import json
+from colorama import Fore, Back, Style 
+
+def main_logo():
+
+    print(" _____           _                 _____ _   _ ") 
+    print("/  ___|         | |               /  __ \ | | |")
+    print("\ `--. _   _ ___| |_ ___ _ __ ___ | /  \/ |_| |")
+    print(" `--. \ | | / __| __/ _ \ '_ ` _ \| |   | __| |")
+    print("/\__/ / |_| \__ \ ||  __/ | | | | | \__/\ |_| |")
+    print("\____/ \__, |___/\__\___|_| |_| |_|\____/\__|_|")
+    print("        __/ |                              ") 
+    print("       |___/                                   ") 
 
 if __name__=="__main__":
-    
+    main_logo()
+    print(Fore.RED + '-----------------++++++++++++++-----------------')    
+    print(Fore.RED + '-----------------Victim Details-----------------') 
+    print(Fore.RED + '-----------------++++++++++++++-----------------')
+    print(Style.RESET_ALL) 
     parser=argparse.ArgumentParser()
     
-    parser.add_argument("-i","--ipaddress",help="IP Address")
+    parser.add_argument("-i","--ipaddress",help="-h")
     
     args=parser.parse_args()
     
@@ -28,4 +44,4 @@ if __name__=="__main__":
     print("\t[+] ZIP\t\t\t",data["zip"])
     print("\t[+] LAT\t\t\t",data["lat"])
     print("\t[+] LON\t\t\t",data["lon"])
-    
+ 
